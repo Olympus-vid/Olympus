8ba4e94e5ad0db83834478078bfc44727f87e7da
brendan <brendan.fay@columbia.edu>
2023-02-01 18:50:49 -0500

    made dependencies script

---
+++ dependencies.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('pip install flask')
+os.system('pip install flask_cors')
+os.system('pip install git+http://github.com/openai/whisper.git')
+os.system('pip install openai')
+os.system('pip install moviepy')
+os.system('pip install foliantcontrib.imagemagick')
+
